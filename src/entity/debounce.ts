5c6f592505f5442dcb6ae86f5294e796370b18f6
olar88 <parkeunyeon18@gmail.com>
2026-03-03 22:14:58 +0800

    Updates

--- src/entity/debounce.ts
+++
@@ -1,8 +0,0 @@
-/** 防抖函數 (Debounce) */
-export const debounce = (func: () => void, delay: number) => {
-  let timeoutId: NodeJS.Timeout;
-  return () => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(func, delay);
-  };
-};
